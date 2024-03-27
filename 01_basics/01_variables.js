c26b32d362e2af81642b569226f0b41d042f9a74
subhampicasso96 <164678505+subhampicasso96@users.noreply.github.com>
2024-03-27 18:31:17 +0000

    Learn variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,18 @@
 const accountID = 12345
-let 
+let accountemail = "shubham@google.com"
+var accountpassword = 98299
+accountcity = "varanasi"
+
+// accountID = 02 // not allowed
+
+accountemail = "Shrivatava@yahoo.com"
+accountpassword = 12345
+accountcity = "Noida"
+
+/*
+ Prefer not to var
+ Becauase of issue in block scope and functional scope
+*/
+
+console.log(accountpassword);
+console.table([accountID, accountemail, accountpassword, accountcity])
