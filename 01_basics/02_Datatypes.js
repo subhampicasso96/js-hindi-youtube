8c5e92c6591a8e9d58194bf30493f3343020120f
subhampicasso96 <164678505+subhampicasso96@users.noreply.github.com>
2024-04-07 11:10:45 +0000

    Data Types

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,9 @@
+"Use Sctrict"// Treat all js code as a newer verson of JS
+
+// alert (3 + 3) // we use node js not browser
+
+console.log (3 + 
+    3) // Code readability that should be high
+
+    Let ("Hitesh")
+    
