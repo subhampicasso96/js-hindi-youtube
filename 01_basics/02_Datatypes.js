60816330c41d71dd3b4e42656632d4afbcec7645
subhampicasso96 <164678505+subhampicasso96@users.noreply.github.com>
2024-04-07 18:41:15 +0000

    datatypes

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -5,5 +5,26 @@
 console.log (3 + 
     3) // Code readability that should be high
 
-    Let ("Hitesh")
+    console.log ("Shubham")
+
+    let = ("Shubham")
+    let age = 28
+    let isloggedin = false
     
+    
+
+
+    // Number
+    // bignet
+    // string
+    // null => Stand alone value type (Object)
+    // Undefined 
+    // Symbol => Unique
+    // 
+
+    // Object => We will discuss this topic more in React
+
+    console.log(typeof "shubham")
+
+    console.log(typeof undefined) // Undefined
+    console.log(typeof null) // object
